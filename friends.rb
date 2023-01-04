35c019cc37a76e7d841b0becf7d5c4fc09d298a0
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-01-04 15:46:31 -0700

    Add states

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ arrays = ["Alec", "Stephen", "Koh", "Logan", "Chris"]
 puts arrays[3]
 puts arrays[4]
 puts arrays[0]
+
+state = ["Illinois", "Colorado", "Iowa", "Vermont", "Washinton"]
+puts state.reverse
